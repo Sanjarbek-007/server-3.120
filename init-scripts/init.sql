e12cf30bf00a51c38dd95b0771298d4563460940
Sanjarbek <sanjarbekabdurahmonov123@gmail.com>
2024-09-13 18:10:21 +0500

    Ready

--- init-scripts/init.sql
+++ init-scripts/init.sql
@@ -1,7 +1,7 @@
 CREATE EXTENSION postgres_fdw;
 
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     username VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL
 );
@@ -10,7 +10,7 @@ CREATE TABLE users (
 CREATE SERVER server1_fdw FOREIGN DATA WRAPPER postgres_fdw OPTIONS (host '3.75.208.130', port '5432', dbname 'server1_db');
 CREATE USER MAPPING FOR postgres SERVER server1_fdw OPTIONS (user 'postgres', password '1111');
 CREATE FOREIGN TABLE users_server1 (
-    id INTEGER,
+    id UUID,
     username VARCHAR(50),
     email VARCHAR(100)
 ) SERVER server1_fdw OPTIONS (schema_name 'public', table_name 'users');
